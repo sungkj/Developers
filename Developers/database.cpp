e14c1ee321d628576db1bc1d0d15e3afc9f6de0e
Joohwan Kim <madali1204@gmail.com>
2022-03-03 18:01:37 +0900

    [Feature] implement mod feature for DB
    Signed-off-by: Joohwan Kim <madali1204@gmail.com>

--- Developers/database.cpp
+++ Developers/database.cpp
@@ -1,6 +1,24 @@
 #include "database.h"
 #include <string>
 
+void modifyInfo(Employee* employee, string column, string val) {
+	if (column.compare("name") == 0) {
+		employee->setName(val);
+	}
+	else if (column.compare("birthday") == 0) {
+		employee->setBirthday(val);
+	}
+	else if (column.compare("cl") == 0) {
+		employee->setCl(val);
+	}
+	else if (column.compare("certi") == 0) {
+		employee->setCerti(val);
+	}
+	else {
+		cout << "ERROR : invalid column" << endl;
+	}
+}
+
 void DataBase::add(Employee* employee) {
 	employees_.push_back(employee);
 }
@@ -72,7 +90,14 @@ vector<Employee*> DataBase::sch(char option, string col, string val) {
 }
 
 void DataBase::mod(vector<Employee*> target, string row, string val) {
-
+	for (int i = 0; i < target.size(); i++) {
+		for (int j = 0; j < employees_.size(); j++) {
+			if (target[i]->getEmployeeNum().compare(employees_[j]->getEmployeeNum()) == 0) {
+				modifyInfo(employees_[j], row, val);
+				break;
+			}
+		}
+	}
 }
 
 vector<Employee*> DataBase::getDBList() {
