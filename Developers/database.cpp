781b6f668e8b3bf01aa77ec3694aefeb288e4e7b
Joohwan Kim <madali1204@gmail.com>
2022-03-03 16:11:31 +0900

    [Feature] implemantate add feature
    Signed-off-by: Joohwan Kim <madali1204@gmail.com>

--- Developers/database.cpp
+++ Developers/database.cpp
@@ -1,7 +1,14 @@
 #include "database.h"
 
 void DataBase::add(Employee* employee) {
+	//int i;
+	//for (i = 0; i < employees_.size(); i++) {
+	//	if (stoi(employees_[i]->getEmployeeNum()) > stoi(employee->getEmployeeNum())) break;
+	//}
 
+	//employees_.insert(employees_.begin() + i, employee);
+	employees_.push_back(employee);
+	
 }
 
 void DataBase::del(vector<Employee*> target) {
