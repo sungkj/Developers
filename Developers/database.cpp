152fc9acfb1c3a5937942858439ada2cd7133020
Joohwan Kim <madali1204@gmail.com>
2022-03-03 17:06:46 +0900

    [Feature] add mod feature and missing del feature
    Signed-off-by: Joohwan Kim <madali1204@gmail.com>

--- Developers/database.cpp
+++ Developers/database.cpp
@@ -1,4 +1,24 @@
 #include "database.h"
+#include <string>
+
+
+void modifyInfo(Employee* employee, string column, string val) {
+	if (column.compare("name") == 0) {
+		employee->setName(val);
+	}
+	else if (column.compare("birthday") == 0) {
+		employee->setBirthday(val);
+	}
+	else if (column.compare("cl") == 0) {
+		employee->setCl(val);
+	}
+	else if (column.compare("certi") == 0) {
+		employee->setCerti(val);
+	}
+	else {
+		cout << "ERROR : invalid column" << endl;
+	}
+}
 
 void DataBase::add(Employee* employee) {
 	//int i;
@@ -12,7 +32,14 @@ void DataBase::add(Employee* employee) {
 }
 
 void DataBase::del(vector<Employee*> target) {
-
+	for (int i = 0; i < target.size(); i++) {
+		for (int j = 0; j < employees_.size(); j++) {
+			if (target[i]->getEmployeeNum().compare(employees_[j]->getEmployeeNum()) == 0) {
+				employees_.erase(employees_.begin() + j);
+				break;
+			}
+		}
+	}
 }
 
 vector<Employee*> DataBase::sch(string row, string val) {
@@ -22,6 +49,15 @@ vector<Employee*> DataBase::sch(string row, string val) {
 }
 
 void DataBase::mod(vector<Employee*> target, string row, string val) {
+	for (int i = 0; i < target.size(); i++) {
+		for (int j = 0; j < employees_.size(); j++) {
+			if (target[i]->getEmployeeNum().compare(employees_[j]->getEmployeeNum()) == 0) {
+				modifyInfo(employees_[j], row, val);
+				break;
+			}
+		}
+	}
+}
 
 vector<Employee*> DataBase::getDBList() {
 	return employees_;
